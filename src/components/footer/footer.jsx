c2b39ba9a0435030c8fa5b3f1905947fac7b3203
malak895 <malak.hammami263@gmail.com>
2023-10-02 17:48:37 +0100

    Message de commit ici

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,10 +1,6 @@
 import "./footer.css";
 import { BsMouse } from "react-icons/bs";
-import { BsInstagram } from "react-icons/bs";
-import { FaFacebookF } from "react-icons/fa";
-import { TiSocialDribbble } from "react-icons/ti";
-import { TiSocialYoutube } from "react-icons/ti";
-import { TiSocialGithub } from "react-icons/ti";
+
 
 function Footer() {
   return (
@@ -17,27 +13,7 @@ function Footer() {
           </h2>
         </a>
       </h1>
-      <div className="social-links">
-        <a href="#">
-          <BsInstagram className="social" />
-        </a>
-        <a href="#">
-          {" "}
-          <FaFacebookF className="social" />
-        </a>
-        <a href="#">
-          {" "}
-          <TiSocialDribbble className="social" />
-        </a>
-
-        <a href="#">
-          <TiSocialYoutube className="social" />
-        </a>
-
-        <a href="#">
-          <TiSocialGithub className="social" />
-        </a>
-      </div>
+    
     </div>
   );
 }
